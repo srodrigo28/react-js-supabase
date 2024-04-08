b7eb285dad1b772213d6c14cd20555ea2e2e8739
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-04-08 20:26:47 -0300

    updated 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import App1 from "./views/App1"
 import Cliente from "./views/Clientes"
-// import Login from "./views/Login"
 
 function App() {
 
   return (
     <>
-      {/* <Cliente /> */}
-      <App1 />
-      {/* <Login /> */}
+      <Cliente />
     </>
   )
 }
